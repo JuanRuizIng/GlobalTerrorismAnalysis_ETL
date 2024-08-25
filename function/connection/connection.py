c9bc18b8276cf688469f551fec8199d43898fd6a
Juan Andrés Ruiz Muñoz <143564687+JuanRuizIng@users.noreply.github.com>
2024-08-25 00:42:50 -0500

    EDA raw creation and update the files to packages
    For further organization and possible main, was to move the file connection to the folder new. And created EDA raw, missing graphics and cleanup for transformed df loading soon

--- function/connection/connection.py
+++ function/connection/connection.py
@@ -1,13 +1,10 @@
 import os
-
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 
 # Reading the environment variables
-load_dotenv("../env/.env")
-
-driver = os.getenv("PG_DRIVER")
+load_dotenv("./env/.env")
 
 user = os.getenv("PG_USER")
 password = os.getenv("PG_PASSWORD")
@@ -17,9 +14,10 @@ port = os.getenv("PG_PORT")
 
 database = os.getenv("PG_DATABASE")
 
+
 # Creating the connection engine from the URL made up of the environment variables
 def creating_engine():
-    url = f"{driver}://{user}:{password}@{host}:{port}/{database}"
+    url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
     engine = create_engine(url)
     
     return engine
