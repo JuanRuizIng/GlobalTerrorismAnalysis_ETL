76bb2f86d3386b1cd4267dc474340cdabf82fe2d
Juan Andrés Ruiz Muñoz <143564687+JuanRuizIng@users.noreply.github.com>
2024-09-04 23:40:15 -0500

    General update
    With packages creation the code is prepared to better automate the process, and in the EDA the new packages are loaded and tested for functionality

---
+++ src/rawLoad/raw.py
@@ -0,0 +1,11 @@
+from src.database.database import creating_engine
+import pandas as pd
+
+def load_raw():
+    """ Load the raw data into the database """
+    df = pd.read_csv('./data/globalterrorismdb_0718dist.csv', low_memory=False, encoding='ISO-8859-1')
+    engine = creating_engine()
+    df.to_sql('global_terrorism_db_raw', engine, if_exists='replace', index=False)
+
+#Check
+load_raw()
