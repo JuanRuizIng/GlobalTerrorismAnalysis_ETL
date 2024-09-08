ba73925f72e2143443a1ff342967ffd0e26abb69
Martín García <martin.garcia@uao.edu.co>
2024-09-07 19:05:45 -0500

    Finished: First task - extract_raw_db

--- dags/tasks/gta_etl.py
+++ dags/tasks/gta_etl.py
@@ -1,6 +1,11 @@
+# Getting the environment variables
+# ----------------------------------
+
 from dotenv import load_dotenv
+
 import sys
 import os
+import logging
 
 load_dotenv(f"{sys.path[0]}/../../env/.env")
 
@@ -9,20 +14,24 @@ email = os.getenv("AIRFLOW_EMAIL")
 
 sys.path.append(f"{path}")
 
+
 # Importing the necessary modules
 # --------------------------------
 
 from src.database.database import creating_engine, create_table
-from src.rawLoad.raw import *
-from src.transformation.transformation import *
 
 import json
+import pandas as pd
+import logging
+import warnings
+
+warnings.simplefilter('ignore', UserWarning)
 
 
 # Creating tasks functions
 # ------------------------
 
-def extract_raw_db() -> json:
+def extract_raw_db():
     """
     Extract the raw data from the database
     
@@ -31,12 +40,30 @@ def extract_raw_db() -> json:
     engine = creating_engine()
 
     try:
-        # Asegurarse de que el engine es compatible con SQLAlchemy
-        df = pd.read_sql_table("global_terrorism_db_cleaned", con=engine)
-        print("Data successfully retrieved.")
+        with engine.connect() as conn:
+            query = 'SELECT * FROM global_terrorism_db_cleaned'
+            
+            df = pd.read_sql_query(query, conn)
+            logging.info("Data successfully retrieved.")
+        
+        
         return df.to_json(orient="records")
     except Exception as e:
         print(f"Error retrieving data: {e}")
 
-# df = extract_raw_db()
-# print(df)
+
+# Example: Loading the data
+def uploading_data(df):
+    engine = creating_engine()
+    
+    try:
+        with engine.connect() as conn:
+            json_data = json.loads(extract_raw_db())
+            df = pd.json_normalize(json_data)
+            df_cropped = df[0:10]
+
+            create_table(conn, df_cropped, "test")
+    except Exception as e:
+        print(f"Error retrieving data: {e}")
+    
+uploading_data(extract_raw_db())
